0b8737e18623be46a0c928fe5485ed96be590540
Konstantin Ignatyev <kgignatyev@gmail.com>
2013-10-29 12:53:42 -0700

    fixed per https://groups.google.com/forum/#!topic/scala-user/wV86VwnKaVk

--- repl-in-war/src/main/scala/kgi/repro/repl_in_war/ReplService.scala
+++ repl-in-war/src/main/scala/kgi/repro/repl_in_war/ReplService.scala
@@ -16,11 +16,23 @@ class ReplService extends InitializingBean {
 
   def afterPropertiesSet() {
 
+    // per thread fixed, still do not like classpath construction at all
+    // https://groups.google.com/forum/#!topic/scala-user/wV86VwnKaVk
 
+    val cl = this.getClass.getClassLoader // or getClassLoader.getParent, or one more getParent...
 
-
+    val urls = cl match {
+      case cl: java.net.URLClassLoader => cl.getURLs.toList
+      case a => sys.error("oops: I was expecting an URLClassLoader, foud a " + a.getClass)
+    }
+    val classpath = urls map {_.toString}
     val settings = new Settings
-    settings.usejavacp.value = true
+    settings.classpath.value = classpath.distinct.mkString(java.io.File.pathSeparator)
+    settings.embeddedDefaults(cl)
+
+
+
+//    settings.usejavacp.value = true
     settings.Yreplsync.value = true
 
 
